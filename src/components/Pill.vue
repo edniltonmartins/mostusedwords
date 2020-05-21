e69e695e90d88a16c8702d82b81d0801c24e4369
edniltonmartins <ednilton17@gmail.com>
2020-05-21 01:01:06 -0300

    first commit

---
+++ src/components/Pill.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <span>{{name}}=</span>
+      <span>{{amount}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['name', 'amount']
+
+}
+</script>
+
+<style>
+
+</style>
